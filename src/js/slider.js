f9c3a81663a1f450094c3287fcec1152c2bfd2b5
MSagdieva <grigorieva.ms@yandex.ru>
2021-10-24 22:02:45 +0300

    fixing some details

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,12 @@
 let slyder = document.querySelector('.slider-text');
-classAdder(slyder, 1);
+for (let i = 1; i<=3; i++){
+classAdder(slyder, i);
+}
 
 function classAdder(elem, i) {
-    elem.querySelector('.text1:nth-child(2)').addEventListener("click", () => {
-        document.querySelector(".main_content .info-block:nth-child(1) .text-container__number").scrollIntoView({ behavior: 'smooth' });
-    })
-    elem.querySelector('.text1:nth-child(3)').addEventListener("click", () => {
-        document.querySelector(".main_content .info-block:nth-child(2) .text-container__number").scrollIntoView({ behavior: 'smooth' });
-    })
-    elem.querySelector('.text1:nth-child(4)').addEventListener("click", () => {
-        document.querySelector(".main_content .info-block:nth-child(3) .text-container__number").scrollIntoView({ behavior: 'smooth' });
-    })
+    elem.querySelector('.text1:nth-child('+(i+1)+')').addEventListener("click", () => {
+        document.querySelector(".main_content .info-block:nth-child("+i+")").classList.add("_active");
+        setTimeout(()=>{document.querySelector(".main_content .info-block:nth-child("+i+") .text-container__number").scrollIntoView({ behavior: 'smooth' })},400);
+        document.querySelector(".slider-indicator").style.top=25*i+"%";
+    });
 }
